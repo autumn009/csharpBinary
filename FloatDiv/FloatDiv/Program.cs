af88c0e2cd6e8b231c7738e52621747a6b673931
Akira Kawamata <autumn@piedey.co.jp>
2023-07-26 14:42:44 +0900

    add FloatDiv

---
+++ FloatDiv/FloatDiv/Program.cs
@@ -0,0 +1,14 @@
+﻿sub(10);
+sub(2);
+
+void sub(float dval)
+{
+    Console.WriteLine($"div and mult by {dval}");
+    float s = 1;
+    float a = s;
+    for (int i = 0; i < 10; i++) a /= dval;
+    for (int i = 0; i < 10; i++) a *= dval;
+    Console.WriteLine($"s={s}");
+    Console.WriteLine($"a={a}");
+    if (s == a) Console.WriteLine("s == a"); else Console.WriteLine("s != a");
+}
