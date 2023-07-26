a990ac0fc0c059d6951ca92d36236be0bb6eb9c4
Akira Kawamata <autumn@piedey.co.jp>
2023-07-26 20:25:42 +0900

    add NumberTypes

---
+++ NumberTypes/NumberTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿sub("byte", byte.MinValue, byte.MaxValue);
+sub("sbyte", sbyte.MinValue, sbyte.MaxValue);
+sub("short", short.MinValue, short.MaxValue);
+sub("ushort", ushort.MinValue, ushort.MaxValue);
+sub("int", int.MinValue, int.MaxValue);
+sub("uint", uint.MinValue, uint.MaxValue);
+sub("long", long.MinValue, long.MaxValue);
+sub("ulong", ulong.MinValue, ulong.MaxValue);
+sub("Half", Half.MinValue, Half.MaxValue);
+sub("float", float.MinValue, float.MaxValue);
+sub("double", double.MinValue, double.MaxValue);
+sub("decimal", decimal.MinValue, decimal.MaxValue);
+
+void sub<T> (string name, T min, T max) where T : IComparable
+{
+    Console.WriteLine($"{name}\t{min}\t{max}");
+}
