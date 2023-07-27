6bb5b0089be64beb3de55d617d4974c5f0b839e0
Akira Kawamata <autumn@piedey.co.jp>
2023-07-27 14:24:39 +0900

    add AddSub

---
+++ AddSub/AddSub/Program.cs
@@ -0,0 +1,12 @@
+﻿byte n = 10;
+Console.WriteLine("*** 1つ増やす ***");
+sub(1);
+Console.WriteLine("*** 1つ減らす ***");
+sub(255);
+
+void sub(byte delta)
+{
+    Console.WriteLine($"{delta}を足します。");
+    n += delta;
+    Console.WriteLine($"nは{n}になりました。");
+}
