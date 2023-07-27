957bc3d8190993938ea072b28f66cffa62c47b4f
Akira Kawamata <autumn@piedey.co.jp>
2023-07-27 09:45:44 +0900

    add MinusUnderflow

---
+++ MinusUnderflow/MinusUnderflow/Program.cs
@@ -0,0 +1,10 @@
+﻿byte a = byte.MinValue + 1;
+for (int i = 0; i < 4; i++)
+{
+    string s = "";
+    if (a == byte.MaxValue) s = "型の最大値です";
+    if (a == byte.MinValue) s = "型の最小値です";
+    Console.WriteLine($"a={a,3} {Convert.ToString(a, 2).PadLeft(8, '0')} {s}");
+    if (i == 3) break;
+    a--; Console.WriteLine("a--;");
+}
