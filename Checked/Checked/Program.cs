c4f8cea7d4e1649320ab630f531256d2336a46cb
Akira Kawamata <autumn@piedey.co.jp>
2023-07-27 10:29:44 +0900

    add Checked

---
+++ Checked/Checked/Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("指定なし");
+byte a = byte.MaxValue;
+a++;
+Console.WriteLine($"a={a}");
+checked
+{
+    try
+    {
+        Console.WriteLine("checked");
+        byte b = byte.MaxValue;
+        b++;
+        Console.WriteLine($"b={b}");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e.Message);
+    }
+}
+unchecked
+{
+    Console.WriteLine("unchecked");
+    byte c = byte.MaxValue;
+    c++;
+    Console.WriteLine($"c={c}");
+}
