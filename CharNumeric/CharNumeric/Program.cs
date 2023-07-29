33bcb0272f86f1d4b868e877091e7334555f5a8b
Akira Kawamata <autumn@piedey.co.jp>
2023-07-29 15:06:54 +0900

    fix

--- CharNumeric/CharNumeric/Program.cs
+++ CharNumeric/CharNumeric/Program.cs
@@ -1,6 +1,6 @@
-﻿    char ch = '0';
-    for (int i = 0; i < 10; i++)
-    {
-        Console.Write(ch);
-        ch++;
-    }
+﻿char ch = '0';
+for (int i = 0; i < 10; i++)
+{
+    Console.Write(ch);
+    ch++;
+}
