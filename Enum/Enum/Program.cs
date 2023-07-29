bf84d774dbe723a4321bbea0f54405522540d72d
Akira Kawamata <autumn@piedey.co.jp>
2023-07-29 19:19:31 +0900

    add Enum

---
+++ Enum/Enum/Program.cs
@@ -0,0 +1,24 @@
+﻿Person[] persons =
+{
+    new Person(){Name="タロー",FavItem= Favourite.ミカンが好き},
+    new Person(){Name="ジロー",FavItem= Favourite.リンゴが好き},
+    new Person(){Name="ハナコ",FavItem= Favourite.メロンが好き},
+};
+
+Console.WriteLine("リンゴが好きな人は");
+foreach (Person person in persons)
+{
+    if( person.FavItem == Favourite.リンゴが好き) Console.WriteLine( person.Name );
+}
+
+enum Favourite
+{
+    ミカンが好き=1,
+    リンゴが好き=2,
+    メロンが好き=3,
+}
+class Person
+{
+    internal string? Name;
+    internal Favourite FavItem;
+}
