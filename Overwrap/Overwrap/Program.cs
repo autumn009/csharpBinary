10ceaf9cb7d1668fe10d5c47bc1d6bbc151206bb
Akira Kawamata <autumn@piedey.co.jp>
2023-07-29 19:51:33 +0900

    add Overwrap

---
+++ Overwrap/Overwrap/Program.cs
@@ -0,0 +1,18 @@
+﻿var logs = new string[256];
+byte logPointer = 0;
+
+for (int i = 0; i < 300; i++) addLog("DATA:" + i);
+dumpLog();
+
+void addLog(string s)
+{
+    logs[logPointer++] = s;
+}
+
+void dumpLog()
+{
+    for (int i = 0; i < 256; i++)
+    {
+        Console.Write($"{logs[--logPointer]},");
+    }
+}
